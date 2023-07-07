b09baa5443d6ecb9337785fb01d48546415e7e59
Ashgan <ashganwiki@gmail.com>
2023-07-07 15:51:42 +0200

    Fix : search functionality for recipes

--- src/components/searchRecipes.vue
+++ src/components/searchRecipes.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="text-4xl font-bold my-5 capitalize items-center flex">
-        <formInput label="Recipe image" type="text" v-model="keyWord" />
+        <formInput label="Search recipes" type="text" v-model="keyWord" @input="s" />
     </div>
-    <ul class="shadow-xl border-2 border-gray-300 p-3 mb-3">
+    <ul v-if="filteredRecipes.length != 0" class="shadow-xl border-2 border-gray-300 p-5 mb-3">
         <li v-for="item in filteredRecipes" :key="item.slug">
             {{ item.name }}
         </li>
@@ -14,6 +14,7 @@ import { computed, ref } from 'vue';
 import formInput from '@/components/formInput.vue';
 import type { Recipe } from '@/types/recipe';
 
+const filteredRecipes = ref<Recipe[]>([])
 const props = defineProps({
     recipes: {
         type: Array as () => Recipe[],
@@ -21,9 +22,11 @@ const props = defineProps({
     }
 });
 
+
 const keyWord = ref<string>('');
-const filteredRecipes = computed(() => {
-    return props.recipes.filter((item) => item.name.toLowerCase().indexOf(keyWord.value.toLowerCase()) !== -1);
-});
+const s = (e: Event) => {
+    filteredRecipes.value = props.recipes.filter((item) => item.name.toLowerCase().indexOf(keyWord.value.toLowerCase()) !== -1);
+    if (keyWord.value === '') filteredRecipes.value = []
+};
 
 </script>
